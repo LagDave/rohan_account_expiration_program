65c6feabe46e5b5214613d8666bab479ac54a61a
rdavephp <rdavephp@gmail.com>
2019-06-30 13:19:24 -0700

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(){
-        $all_users = User::all();
+        $all_users = DB::select("SELECT * FROM RohanUser.dbo.TUser");
         return $all_users;
     }
 }
